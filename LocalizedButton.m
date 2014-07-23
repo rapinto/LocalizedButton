cab980c063a8af5fb4e742b303f25efc12dda8f3
Raphael Pinto <rpinto@dealabs.com>
2014-07-23 13:50:18 +0200

    improve the way button behave when selected in a uitableviewcell

--- LocalizedButton.m
+++ LocalizedButton.m
@@ -109,4 +109,31 @@
 }
 
 
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+        
+    [NSOperationQueue.mainQueue addOperationWithBlock:^{ self.highlighted = YES; }];
+}
+
+
+- (void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesCancelled:touches withEvent:event];
+    
+    [self performSelector:@selector(setDefault) withObject:nil afterDelay:0.1];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+    
+    [self performSelector:@selector(setDefault) withObject:nil afterDelay:0.1];
+}
+
+
+- (void)setDefault
+{
+    [NSOperationQueue.mainQueue addOperationWithBlock:^{ self.highlighted = NO; }];
+}
 @end
